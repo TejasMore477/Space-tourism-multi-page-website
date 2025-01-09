c609417d2af09eda3cca2b9008a4c9513155452a
Tejas More <tejasmore660@gmail.com>
2025-01-09 11:22:34 +0530

    opacity 20 to 15 in navbar

--- src/Components/partials/Navbar.jsx
+++ src/Components/partials/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router";
 
 function Navbar({ setMenuBar }) {
   return (
-    <div className="w-full bg-zinc-600 bg-opacity-20 backdrop-blur-sm flex items-center justify-between px-4 py-2">
+    <div className="w-full bg-zinc-600 bg-opacity-15 backdrop-blur-sm flex items-center justify-between px-4 py-2">
       <Link to="/">
         <img className="size-[9vw] cursor-pointer" src={logo} />
       </Link>
