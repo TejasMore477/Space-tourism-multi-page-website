023fad6a7b0cacab86bb34a29f54a39c70b60889
Tejas More <tejasmore660@gmail.com>
2025-01-09 11:12:27 +0530

    despinaiton page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Route, Routes } from "react-router";
 import Home from "./Components/Home";
 import SideMenu from './Components/partials/SideMenu';
+import Destination from "./Components/Destination";
 
 function App() {
   const [menuBar, setMenuBar] = useState(false);
@@ -11,6 +12,7 @@ function App() {
       <SideMenu setMenuBar={setMenuBar} menuBar={menuBar}/>
       <Routes>
         <Route path="/" element={<Home setMenuBar={setMenuBar} />} />
+        <Route path="/destination" element={<Destination setMenuBar={setMenuBar} />} />
       </Routes>
     </div>
   );
---
+++ src/Components/Destination.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./partials/Navbar";
+import json from "../data.json";
+
+function Destination({ setMenuBar }) {
+  const destData = json.destinations;
+  const [di, setDi] = useState(0);
+
+  useEffect(()=>{
+     console.log("comp mount");
+  },[])
+
+  return (
+    <div
+      style={{ backgroundImage: `url('/destination/background-destination-mobile.jpg')` }}
+      className="px-2 pt-5 w-full h-screen bg-cover bg-center bg-no-repeat"
+    >
+      <Navbar setMenuBar={setMenuBar} />
+      <div className="w-full h-[88vh] text-white flex flex-col items-center justify-around pt-3 px-6 pb-10">
+        <p className="text-[3vw] font-light text-center">01 Pick your destination</p>
+        
+        <img className="size-[40vw] mt-5" src={destData[di].images.png} alt={destData[di].name} />
+        
+        <div className="flex items-center justify-center gap-5 mt-4">
+          {destData.map((e, i) => (
+            <button
+              key={i}
+              onClick={() => setDi(i)}
+              className={`text-[3vw] font-light ${di === i ? "text-white border-b-[1px]" : "text-gray-400"}`}
+            >
+              {e.name}
+            </button>
+          ))}
+        </div>
+
+        <div className="mt-5 text-center">
+          <h2 className=" mb-5 text-[10vw] uppercase font-light leading-none">{destData[di].name}</h2>
+          <p className=" h-[10vh] mb-5 text-[3vw] leading-[17px] font-light mt-2 px-4">{destData[di].description}</p>
+          <hr  className="border-none h-[1.5px] bg-zinc-600 mt-2"/>
+          <h3 className=" mt-5 text-[3.3vw] text-zinc-400 font-semibold uppercase leading-none tracking-tighter py-2">  Avg. distance</h3>
+          <h2 className="text-[5.3vw] tracking-tight font-semibold italic leading-none">{destData[di].distance}</h2>
+          <h3 className=" mt-5 text-[3.3vw] text-zinc-400 font-semibold uppercase leading-none tracking-tighter py-2">Est. travel time</h3>
+          <h2 className="text-[5.3vw] tracking-tight font-semibold italic leading-none">{destData[di].travel}</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Destination;
