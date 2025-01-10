140bb5aa9f668e1d5edb4795e003bbce970f54d7
Tejas More <tejasmore660@gmail.com>
2025-01-10 10:10:52 +0530

    nav sm responsive

--- src/Components/partials/Navbar.jsx
+++ src/Components/partials/Navbar.jsx
@@ -5,13 +5,13 @@ import { Link } from "react-router";
 
 function Navbar({ setMenuBar }) {
   return (
-    <div className="w-full bg-zinc-600 bg-opacity-15 backdrop-blur-sm flex items-center justify-between px-4 py-2">
+    <div className="w-full md:bg-fuchsia-400 bg-zinc-600 bg-opacity-15 backdrop-blur-sm flex items-center justify-between sm:px-10 px-4 sm:py-0 py-2">
       <Link to="/">
-        <img className="size-[9vw] cursor-pointer" src={logo} />
+        <img className="sm:size-[5vw] size-[9vw] cursor-pointer" src={logo} />
       </Link>
       <img
         onClick={() => setMenuBar((prev) => !prev)}
-        className="size-[3.5vw] cursor-pointer"
+        className="sm:size-[2.5vw] size-[3.5vw] cursor-pointer"
         src={menu}
       />
     </div>
