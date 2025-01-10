33c7db525cbad77ae81ce488392cd4c309ad0e6b
Tejas More <tejasmore660@gmail.com>
2025-01-10 10:12:45 +0530

    home sm responsive

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,13 +7,13 @@ function Home({ setMenuBar }) {
   return (
     <div
       style={{ backgroundImage: `url(${mobBG})` }}
-      className={`px-2 pt-5 w-full h-screen bg-cover bg-center bg-no-repeat`}
+      className="px-2 pt-5 w-full h-screen bg-cover bg-center bg-no-repeat"
     >
       <Navbar setMenuBar={setMenuBar} />
-      <div className="text-white flex flex-col items-center justify-center pt-10 h-[85vh] bg-red-9000 px-10">
-        <p className="text-[4vw] font-thin">So, you want to travel to</p>
+      <div className="text-white flex flex-col items-center justify-center pt-10 h-[85vh] px-10">
+        <p className="sm:text-[2.7vw] text-[4vw] font-thin">So, you want to travel to</p>
         <h1 className="text-[18vw] uppercase font-mdium">Space</h1>
-        <p className="text-center text-[4.3vw] font-light leading-tight">
+        <p className="text-center sm:text-[2.5vw] sm:px-10 text-[4.3vw] font-light leading-tight">
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
@@ -21,12 +21,12 @@ function Home({ setMenuBar }) {
         </p>
         <Link
           to="/destination"
-          className="size-[37vw] text-[3.5vw] grid place-content-center rounded-full text-black font-semibold mt-20 bg-white animate-pulse"
+          className="sm:size-[20vw] size-[37vw] sm:text-[2.5vw] text-[3.5vw] grid place-content-center rounded-full text-black font-semibold mt-20 bg-white animate-pulse"
         >
           Explore
         </Link>
-        <div className=" w-full text-center font-semibold text-sm leading-tight mt-5">
-          Challenge by{" "}
+        <div className="w-full text-center font-semibold sm:text-[1.7vw] text-[2.5vw] leading-tight sm:mt-20 mt-5">
+          Challenge by
           <a
             className="text-black"
             href="https://www.frontendmentor.io?ref=challenge"
@@ -34,7 +34,7 @@ function Home({ setMenuBar }) {
           >
             Frontend Mentor
           </a>
-          . Coded by{" "}
+          . Coded by
           <a
             href=""
             className="text-black"
